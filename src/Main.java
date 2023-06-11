edc07f70a1c02f2a454c81c6939dffaa29d07cf6
nikbatura <nikbatura@mail.ru>
2023-06-11 23:50:16 +0700

    Проверьте дз

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@ public class Main {
 
 
     public static void main(String[] args) {
+        Metods.printSeparator();
         Gryffindor harryPotter = new Gryffindor("Harry Potter", 65, 74, 60, 78, 90);
         Gryffindor hermioneGranger = new Gryffindor("Hermione Granger", 95, 88, 54, 61, 72);
         Gryffindor ronWeasley = new Gryffindor("Ron Weasley", 52, 50, 68, 65, 85);
         System.out.println(harryPotter);
         System.out.println(hermioneGranger);
         System.out.println(ronWeasley);
+        ronWeasley.compare(harryPotter);
         Metods.printSeparator();
         Slytherin dracoMalfoy = new Slytherin("Draco Malfoy", 63, 74, 86, 44, 79, 45);
         Slytherin grahamMontague = new Slytherin("Graham Montague", 51, 53, 46, 81, 63, 79);
@@ -16,6 +18,7 @@ public class Main {
         System.out.println(dracoMalfoy);
         System.out.println(grahamMontague);
         System.out.println(gregoryGoyle);
+
         Metods.printSeparator();
         Рufflepuff zachariahSmith = new Рufflepuff("Zachariah Smith", 51, 44, 78, 56, 40);
         Рufflepuff cedricDiggory = new Рufflepuff(" Cedric Diggory", 77, 66, 90, 65, 89);
@@ -23,6 +26,7 @@ public class Main {
         System.out.println(zachariahSmith);
         System.out.println(cedricDiggory);
         System.out.println(justinFinchFletchley);
+
         Metods.printSeparator();
         Ravenclaw zhouChang = new Ravenclaw("Zhou Chang", 69, 72, 87, 56, 45, 66);
         Ravenclaw padmaPatil = new Ravenclaw("Padma Patil", 55, 67, 96, 35, 46, 87);
@@ -30,7 +34,14 @@ public class Main {
         System.out.println(zhouChang);
         System.out.println(padmaPatil);
         System.out.println(marcusBelby);
+
+
         Metods.printSeparator();
+        hermioneGranger.compare(dracoMalfoy);
+        cedricDiggory.compare(zhouChang);
+
+
+
 
 
     }
--- src/Ravenclaw.java
+++ src/Ravenclaw.java
@@ -45,11 +45,20 @@ public class Ravenclaw extends Hogwarts{
     }
 
     @Override
+    int calculateSpecificScore() {
+        return this.intelligence + this.wisdom + this.wit + this.creativity;
+    }
+    @Override
+    void printCompareOfStudents(Hogwarts best, Hogwarts worst) {
+        System.out.println(String.format("%s лучший Когтевранец, чем %s", best.getName(), worst.getName()));
+    }
+
+    @Override
     public String toString() {
-        return "Когтевран: " + " Имя - " + getName() + ", Сила магии - " + getPowerMagic() + ", Трансгрессия - " + getTransgression() +
+        return "Ravenclaw: " + super.toString() +
                 ", Ум - " + intelligence +
                 ", Мудрость - " + wisdom +
                 ", Остроумие - " + wit +
-                ", Творчество - " + creativity;
+                ", Творчесво - " + creativity;
     }
 }
